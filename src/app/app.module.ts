23b914041cb742b185e4c4872f4255545f08896a
dxquyet16 <quyetcoder16@gmail.com>
2024-06-08 16:55:02 +0700

    done p1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 
 @NgModule({
@@ -14,12 +18,18 @@ import { OrderComponent } from './order/order.component';
     HeaderComponent,
     FooterComponent,
     OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailProductComponent,
 
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    DetailProductComponent
+  ]
 })
 export class AppModule { }
