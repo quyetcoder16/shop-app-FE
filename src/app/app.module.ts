14b501ac26d8c6af6211a5497f573b5ae3bb48ca
dxquyet16 <quyetcoder16@gmail.com>
2024-06-08 15:42:51 +0700

    error app-order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
   declarations: [
 
 
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+
   ],
   imports: [
     BrowserModule
